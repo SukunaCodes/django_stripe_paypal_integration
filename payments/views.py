6022175287af1bd49c427d423a3fa116dbc83ffd
Forbidden Programmer <67829913+SukunaCodes@users.noreply.github.com>
2024-09-27 13:09:10 +0300

    Initialized django app

---
+++ payments/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Donation Page
+def donation(request):
+    pass
+
+# Successful payment
+def successful_payment(request):
+    pass
+
+# Failed payment
+def failed_payment(request):
+    pass
