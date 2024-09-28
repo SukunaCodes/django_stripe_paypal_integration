71523036200224413c1056051919abe66ae92f60
Forbidden Programmer <67829913+SukunaCodes@users.noreply.github.com>
2024-09-28 09:56:58 +0300

    defined payment urls

--- payments/urls.py
+++ payments/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-
+from . import views
 
 urlpatterns = [
-
+    path('', views.donation, name=''),
+    path('payment_success', views.successful_payment, name='payment_success'),
+    path('payment_failed', views.failed_payment, name='payment_failed'),
 ]
