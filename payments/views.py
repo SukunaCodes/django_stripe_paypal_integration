d4b750c3d71de407025f4080ee2e94949a21d3af
Forbidden Programmer <67829913+SukunaCodes@users.noreply.github.com>
2024-09-30 13:11:35 +0300

    further adjustemnt to the donation function

--- payments/views.py
+++ payments/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 
+from payments.models import Donation
+
+
 # Donation Page
 def donation(request):
-    return render(request, 'donation/my_donation.html')
+    donation = Donation.objects.get(id=2)
+    context = {'donation': donation}
+    return render(request, 'donation/my_donation.html', context=context)
+
+
 # Successful payment
 def successful_payment(request):
     return render(request, 'donation/payment_success.html')
+
+
 # Failed payment
 def failed_payment(request):
     return render(request, 'donation/payment_failed.html')
